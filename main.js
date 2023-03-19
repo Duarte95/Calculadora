602a80237eb93fa7d60eaf892f75ff80a5c68be8
Duarte95 <122475491+Duarte95@users.noreply.github.com>
2023-03-19 15:32:08 -0430

    Creando archivo Calculadora

---
+++ main.js
@@ -0,0 +1,19 @@
+var display = document.querySelector('#display');
+let ingreso = document.querySelector('button')
+
+function sumar(){
+  
+}
+
+function restar() {
+  // body...
+}
+
+function dividir(){
+  
+}
+
+function multiplicar(){
+  
+}
+
